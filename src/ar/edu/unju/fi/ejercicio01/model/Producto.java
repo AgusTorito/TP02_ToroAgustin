9d28e7417ffbac4b51c3e42029455ca249421e52
Agus <Agus@DESKTOP-MNBU4VL>
2024-04-29 17:50:11 -0300

    Ejercicio01_Tp02

--- src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -5,7 +5,20 @@ public class Producto {
 	private String codigo;
 	private String descripcion;
 	private double precioU;
+	private OrigenFabricacion origenfabricacion;
+	private Categoria categoria;
 	
+	public Producto() {
+		
+	}
+	
+	public Producto(String codigo, String descripcion, double precioU, OrigenFabricacion origenfabricacion, Categoria categoria) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioU = precioU;
+		this.origenfabricacion = origenfabricacion;
+		this.categoria = categoria;
+	}
 	
 	public String getCodigo() {
 		return codigo;
@@ -24,6 +37,30 @@ public class Producto {
 	}
 	public void setPrecioU(double precioU) {
 		this.precioU = precioU;
+	}
+	
+	public enum OrigenFabricacion {
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	
+	public enum Categoria {
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+
+	public OrigenFabricacion getOrigenfabricacion() {
+		return origenfabricacion;
+	}
+
+	public void setOrigenfabricacion(OrigenFabricacion origenfabricacion) {
+		this.origenfabricacion = origenfabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 	
 	
